3cbd25afcc8742db5af1e096f8a65cf22b22ca00
first repo <r.zamuruev@vk.com>
2023-07-14 18:33:01 +0300

    add SubjectRepository findSubjectsByTeachers

--- src/main/java/school/practice/repositories/SubjectRepository.java
+++ src/main/java/school/practice/repositories/SubjectRepository.java
@@ -1,8 +1,18 @@
 package school.practice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import school.practice.models.Subject;
+import school.practice.models.Teacher;
+
+import java.util.*;
+
 @Repository
-public interface SubjectRepository extends JpaRepository<Subject,Long> {
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    @Query("SELECT DISTINCT s FROM Subject s JOIN s.teachers t WHERE t IN :teachers")
+    List<Subject> findSubjectsByTeachers(@Param("teachers") Set<Teacher> teachers);
+
+
 }
